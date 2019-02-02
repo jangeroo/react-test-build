4171bf7de9385341359aac0389fb6185eaa6c4d2
Michael Jang <michaeljang86@gmail.com>
2019-02-02 16:41:22 -0500

    Updates

---
+++ precache-manifest.e0dca7a20623d0b73e09bbcce28c045c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "648d02a1c3aabd4f4cc1",
+    "url": "/react-test-build/static/css/main.5798c2b0.chunk.css"
+  },
+  {
+    "revision": "648d02a1c3aabd4f4cc1",
+    "url": "/react-test-build/static/js/main.648d02a1.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/react-test-build/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "3e169481a020ab0b6bc8",
+    "url": "/react-test-build/static/js/runtime~main.3e169481.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-test-build/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad593e0e80bfa280e84be973b2178622",
+    "url": "/react-test-build/index.html"
+  }
+];
